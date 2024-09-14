bcb2c922e689c4fcc3704f9c66a81f7163aa1f39
svyatoslavkachmar-cnic <Svyatoslav.Kachmar@centralnic.com>
2024-09-14 23:01:38 +0300

    initial commit

--- main.ts
+++ main.ts
@@ -1,85 +1,71 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Plugin, PluginSettingTab, Setting } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
-
+// Plugin settings interface
 interface MyPluginSettings {
-	mySetting: string;
+	interval: number;
+	backgrounds: string[];
 }
 
 const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+	interval: 3000, // 3 seconds
+	backgrounds: ["url('https://img.freepik.com/free-photo/geometric-shapes-orange-background_23-2148209958.jpg?size=626&ext=jpg&ga=GA1.1.1174352020.1726297089&semt=ais_hybrid')", "url('https://img.freepik.com/free-photo/geometric-shapes-orange-background_23-2148209958.jpg?size=626&ext=jpg&ga=GA1.1.1174352020.1726297089&semt=ais_hybrid')"],
+};
 
 export default class MyPlugin extends Plugin {
 	settings: MyPluginSettings;
+	intervalId: number;
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+		// Start changing backgrounds
+		this.startBackgroundChange();
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+		// Add a settings tab
+		this.addSettingTab(new MyPluginSettingTab(this.app, this));
+	}
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+	startBackgroundChange() {
+		let index = 0;
+		this.intervalId = window.setInterval(() => {
+			console.log(this.settings.backgrounds[index]);
+			const titleBarTexts = document.querySelectorAll(".view-content") as NodeListOf<HTMLElement>;
+			titleBarTexts.forEach((titleBarText, index) => {
+				// Apply the background image
+				titleBarText.style.backgroundImage = `${this.settings.backgrounds[index]}`;
+				titleBarText.style.backgroundSize = "cover";
+				titleBarText.style.backgroundPosition = "center";
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
+				// Ensure position relative for the overlay and content
+				titleBarText.style.position = "relative";
+				titleBarText.style.zIndex = "2";  // Ensure content is above the overlay
+
+				// Check if overlay already exists by class name
+				if (!titleBarText.querySelector('.background-overlay')) {
+					// Create an overlay div for opacity if it doesn't exist
+					const overlay = document.createElement("div");
+					overlay.className = "background-overlay"; // Add a class for easy identification
+					overlay.style.position = "absolute";
+					overlay.style.top = "0";
+					overlay.style.left = "0";
+					overlay.style.width = "100%";
+					overlay.style.height = "100%";
+					overlay.style.backgroundColor = "rgba(255, 255, 255, 0.5)";  // White overlay with 85% opacity
+					overlay.style.zIndex = "-1";  // Ensure overlay is below the content
+					overlay.style.pointerEvents = "none";  // Makes the overlay non-interactive
+
+					// Append the overlay to the titleBarText
+					titleBarText.appendChild(overlay);
 				}
-			}
-		});
+			});
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+			index = (index + 1) % this.settings.backgrounds.length;
+		}, this.settings.interval);
 	}
 
 	onunload() {
-
+		// Clear the interval when the plugin is unloaded
+		window.clearInterval(this.intervalId);
 	}
 
 	async loadSettings() {
@@ -91,23 +77,7 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
+class MyPluginSettingTab extends PluginSettingTab {
 	plugin: MyPlugin;
 
 	constructor(app: App, plugin: MyPlugin) {
@@ -116,18 +86,31 @@ class SampleSettingTab extends PluginSettingTab {
 	}
 
 	display(): void {
-		const {containerEl} = this;
+		const { containerEl } = this;
 
 		containerEl.empty();
 
+		containerEl.createEl('h2', { text: 'Background Switcher Settings' });
+
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
+			.setName('Switch Interval (ms)')
+			.setDesc('Time between background changes in milliseconds.')
 			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
+				.setPlaceholder('Enter time in ms')
+				.setValue(this.plugin.settings.interval.toString())
 				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
+					this.plugin.settings.interval = parseInt(value);
+					await this.plugin.saveSettings();
+				}));
+
+		new Setting(containerEl)
+			.setName('Background Images')
+			.setDesc('Comma-separated list of image URLs or colors.')
+			.addTextArea(text => text
+				.setPlaceholder('Enter image URLs or colors')
+				.setValue(this.plugin.settings.backgrounds.join(', '))
+				.onChange(async (value) => {
+					this.plugin.settings.backgrounds = value.split(',').map(v => v.trim());
 					await this.plugin.saveSettings();
 				}));
 	}
