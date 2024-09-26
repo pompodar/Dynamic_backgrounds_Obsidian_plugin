8db41684c25d647aa765f6bc38d0ac6c77226228
svyatoslavkachmar-cnic <Svyatoslav.Kachmar@centralnic.com>
2024-09-27 00:07:01 +0300

    switch from setinterval to just random backgrounds

--- main.ts
+++ main.ts
@@ -1,11 +1,8 @@
-import { App, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { Plugin, PluginSettingTab, App, Setting } from 'obsidian';
 
-// Plugin settings interface
 interface MyPluginSettings {
-	interval: number;
 	backgrounds: string[];
 	opacitySettings: {
-		sidebar: number;
 		viewContent: number;
 		tabHeaderContainer: number;
 		tabHeader: number;
@@ -15,34 +12,38 @@ interface MyPluginSettings {
 }
 
 const DEFAULT_SETTINGS: MyPluginSettings = {
-	interval: 3000, // 3 seconds
 	backgrounds: [
-		"url('https://img.freepik.com/free-photo/geometric-shapes-orange-background_23-2148209958.jpg?size=626&ext=jpg&ga=GA1.1.1174352020.1726297089&semt=ais_hybrid')",
-		"url('https://img.freepik.com/free-photo/geometric-shapes-orange-background_23-2148209958.jpg?size=626&ext=jpg&ga=GA1.1.1174352020.1726297089&semt=ais_hybrid')",
+		"https://example.com/image1.jpg",
+		"https://example.com/image2.jpg",
+		"https://example.com/image3.jpg"
 	],
 	opacitySettings: {
-		sidebar: 0.5,
 		viewContent: 0.5,
-		tabHeaderContainer: 0.5,
-		tabHeader: 0.5,
-		workspaceLeaf: 0.5,
-		kanbanItem: 0.2,
-	},
+		tabHeaderContainer: 0.4,
+		tabHeader: 0.3,
+		workspaceLeaf: 0.6,
+		kanbanItem: 0.7
+	}
 };
 
 export default class MyPlugin extends Plugin {
 	settings: MyPluginSettings;
-	intervalId: number;
-	currentBackgroundIndex: number = 0;
 
 	async onload() {
 		await this.loadSettings();
 
-		// Start changing backgrounds
-		this.startBackgroundChange();
-
 		// Add a settings tab
 		this.addSettingTab(new MyPluginSettingTab(this.app, this));
+
+		// Add command to trigger background change
+		this.addCommand({
+			id: 'change-background',
+			name: 'Change Background',
+			callback: () => this.updateBackgrounds(),
+		});
+
+		// Initial background setup
+		this.updateBackgrounds();
 
 		// Listen for changes to the active leaf
 		this.registerEvent(this.app.workspace.on('active-leaf-change', () => {
@@ -50,14 +51,16 @@ export default class MyPlugin extends Plugin {
 		}));
 	}
 
-	startBackgroundChange() {
-		this.intervalId = window.setInterval(() => {
-			this.currentBackgroundIndex = (this.currentBackgroundIndex + 1) % this.settings.backgrounds.length;
-			this.updateBackgrounds();
-		}, this.settings.interval);
+	getRandomBackground(): string {
+		// Get a random background from the settings
+		const backgrounds = this.settings.backgrounds;
+		const randomIndex = Math.floor(Math.random() * backgrounds.length);
+		return backgrounds[randomIndex];
 	}
 
-	applyBackgroundAndOverlay(elements: NodeListOf<HTMLElement>, backgroundImageUrl: string, opacity: number) {
+	applyBackgroundAndOverlay(elements: NodeListOf<HTMLElement>, opacity: number) {
+		const backgroundImageUrl = this.getRandomBackground();
+
 		elements.forEach((element) => {
 			// Apply the background image
 			element.style.backgroundImage = `url("${backgroundImageUrl}")`;
@@ -78,7 +81,6 @@ export default class MyPlugin extends Plugin {
 				overlay.style.left = "0";
 				overlay.style.width = "100%";
 				overlay.style.height = "100%";
-
 				overlay.style.backgroundColor = `rgba(255, 255, 255, ${opacity})`;  // Apply the configured opacity
 				overlay.style.zIndex = "-1";  // Ensure overlay is below the content
 				overlay.style.pointerEvents = "none";  // Makes the overlay non-interactive
@@ -90,20 +92,18 @@ export default class MyPlugin extends Plugin {
 	}
 
 	updateBackgrounds() {
-		const backgroundImageUrl = this.settings.backgrounds[this.currentBackgroundIndex]; // Use the current background image from settings
-
-		// Apply background with configured opacity to different elements
+		// Apply random background to different elements with configured opacity
 		const titleBarTexts = document.querySelectorAll(".view-content") as NodeListOf<HTMLElement>;
-		this.applyBackgroundAndOverlay(titleBarTexts, backgroundImageUrl, this.settings.opacitySettings.viewContent);
+		this.applyBackgroundAndOverlay(titleBarTexts, this.settings.opacitySettings.viewContent);
 
 		const headerContainers = document.querySelectorAll(".workspace-tab-header-container") as NodeListOf<HTMLElement>;
-		this.applyBackgroundAndOverlay(headerContainers, backgroundImageUrl, this.settings.opacitySettings.tabHeaderContainer);
+		this.applyBackgroundAndOverlay(headerContainers, this.settings.opacitySettings.tabHeaderContainer);
 
 		const headerTabs = document.querySelectorAll(".workspace-tab-header") as NodeListOf<HTMLElement>;
-		this.applyBackgroundAndOverlay(headerTabs, backgroundImageUrl, this.settings.opacitySettings.tabHeader);
+		this.applyBackgroundAndOverlay(headerTabs, this.settings.opacitySettings.tabHeader);
 
 		const workspaceLeaf = document.querySelectorAll(".workspace-leaf") as NodeListOf<HTMLElement>;
-		this.applyBackgroundAndOverlay(workspaceLeaf, backgroundImageUrl, this.settings.opacitySettings.workspaceLeaf);
+		this.applyBackgroundAndOverlay(workspaceLeaf, this.settings.opacitySettings.workspaceLeaf);
 
 		// Use a single MutationObserver to handle kanban and other dynamically added elements
 		const observer = new MutationObserver((mutationsList) => {
@@ -113,13 +113,13 @@ export default class MyPlugin extends Plugin {
 					// Check for kanban lanes
 					const kanbanLane = document.querySelectorAll(".kanban-plugin__lane") as NodeListOf<HTMLElement>;
 					if (kanbanLane.length > 0) {
-						this.applyBackgroundAndOverlay(kanbanLane, backgroundImageUrl, this.settings.opacitySettings.viewContent);
+						this.applyBackgroundAndOverlay(kanbanLane, this.settings.opacitySettings.viewContent);
 					}
 
 					// Check for kanban items
 					const kanbanItems = document.querySelectorAll(".kanban-plugin__item-title-wrapper") as NodeListOf<HTMLElement>;
 					if (kanbanItems.length > 0) {
-						this.applyBackgroundAndOverlay(kanbanItems, backgroundImageUrl, this.settings.opacitySettings.kanbanItem);
+						this.applyBackgroundAndOverlay(kanbanItems, this.settings.opacitySettings.kanbanItem);
 					}
 				}
 			});
@@ -129,10 +129,8 @@ export default class MyPlugin extends Plugin {
 		observer.observe(document.body, { childList: true, subtree: true });
 	}
 
-
 	onunload() {
-		// Clear the interval when the plugin is unloaded
-		window.clearInterval(this.intervalId);
+		// Clean up resources when the plugin is unloaded
 	}
 
 	async loadSettings() {
@@ -144,6 +142,7 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
+// Settings tab class for adjusting backgrounds and opacity settings
 class MyPluginSettingTab extends PluginSettingTab {
 	plugin: MyPlugin;
 
@@ -154,159 +153,40 @@ class MyPluginSettingTab extends PluginSettingTab {
 
 	display(): void {
 		const { containerEl } = this;
-
 		containerEl.empty();
 
-		containerEl.createEl('h2', { text: 'Background Switcher Settings' });
+		containerEl.createEl('h2', { text: 'Settings for Random Background Plugin' });
 
-		// Setting for switch interval
-		new Setting(containerEl)
-			.setName('Switch Interval (ms)')
-			.setDesc('Time between background changes in milliseconds.')
-			.addText(text => text
-				.setPlaceholder('Enter time in ms')
-				.setValue(this.plugin.settings.interval.toString())
-				.onChange(async (value) => {
-					this.plugin.settings.interval = parseInt(value);
-					await this.plugin.saveSettings();
-
-					// Clear the old interval and restart the background switcher with the new interval
-					window.clearInterval(this.plugin.intervalId);
-					this.plugin.startBackgroundChange();  // Restart the interval with new settings
-
-					this.plugin.updateBackgrounds();  // Reload backgrounds immediately after the change
-				}));
-
-		// Setting for background images
+		// Add a text field for each background URL
 		new Setting(containerEl)
 			.setName('Background Images')
-			.setDesc('Comma-separated list of image URLs or colors.')
-			.addTextArea(text => text
-				.setPlaceholder('Enter image URLs or colors')
+			.setDesc('Add URLs for background images')
+			.addTextArea((text) => text
+				.setPlaceholder('Enter URLs separated by commas')
 				.setValue(this.plugin.settings.backgrounds.join(', '))
 				.onChange(async (value) => {
-					this.plugin.settings.backgrounds = value.split(',').map(v => v.trim());
+					this.plugin.settings.backgrounds = value.split(',').map(url => url.trim());
 					await this.plugin.saveSettings();
-
-					this.plugin.updateBackgrounds();  // Reload backgrounds after changing the background images
 				}));
 
-		const titleBarTexts = document.querySelectorAll(".view-content") as NodeListOf<HTMLElement>;
+		// Add sliders for adjusting opacity settings
+		this.addOpacitySetting(containerEl, 'viewContent', 'View Content Opacity');
+		this.addOpacitySetting(containerEl, 'tabHeaderContainer', 'Tab Header Container Opacity');
+		this.addOpacitySetting(containerEl, 'tabHeader', 'Tab Header Opacity');
+		this.addOpacitySetting(containerEl, 'workspaceLeaf', 'Workspace Leaf Opacity');
+		this.addOpacitySetting(containerEl, 'kanbanItem', 'Kanban Item Opacity');
+	}
 
-		// Add settings for opacity per element
+	addOpacitySetting(containerEl: HTMLElement, key: keyof MyPluginSettings['opacitySettings'], name: string) {
 		new Setting(containerEl)
-			.setName('Sidebar Opacity')
-			.setDesc('Opacity for the sidebar background overlay.')
-			.addSlider(slider => slider
+			.setName(name)
+			.addSlider((slider) => slider
 				.setLimits(0, 1, 0.1)
-				.setValue(this.plugin.settings.opacitySettings.sidebar)
+				.setValue(this.plugin.settings.opacitySettings[key])
 				.onChange(async (value) => {
-					this.plugin.settings.opacitySettings.sidebar = value;
-
-					const headerOverlayContainers = document.querySelectorAll(".workspace-tab-header-container .background-overlay") as NodeListOf<HTMLElement>;
-					headerOverlayContainers.forEach(cont => {
-						cont.style.background = `rgba(255, 255, 255, ${value})`;
-					});
-
+					this.plugin.settings.opacitySettings[key] = value;
 					await this.plugin.saveSettings();
-					this.plugin.updateBackgrounds();  // Reload backgrounds after changing opacity
-				}));
-
-		// Add settings for opacity per element
-		new Setting(containerEl)
-			.setName('View Content Opacity')
-			.setDesc('Opacity for the view content background overlay.')
-			.addSlider(slider => slider
-				.setLimits(0, 1, 0.1)
-				.setValue(this.plugin.settings.opacitySettings.viewContent)
-				.onChange(async (value) => {
-					this.plugin.settings.opacitySettings.viewContent = value;
-
-					const headerOverlayContainers = document.querySelectorAll(".workspace-tab-header-container .background-overlay") as NodeListOf<HTMLElement>;
-					headerOverlayContainers.forEach(cont => {
-						cont.style.background = `rgba(255, 255, 255, ${value})`;
-					});
-
-					await this.plugin.saveSettings();
-					this.plugin.updateBackgrounds();  // Reload backgrounds after changing opacity
-				}));
-
-		// Setting for tab header container opacity
-		new Setting(containerEl)
-			.setName('Tab Header Container Opacity')
-			.setDesc('Opacity for the tab header container background overlay.')
-			.addSlider(slider => slider
-				.setLimits(0, 1, 0.1)
-				.setValue(this.plugin.settings.opacitySettings.tabHeaderContainer)
-				.onChange(async (value) => {
-					this.plugin.settings.opacitySettings.tabHeaderContainer = value;
-
-					const headerOverlayTabs = document.querySelectorAll(".workspace-tab-header .background-overlay") as NodeListOf<HTMLElement>;
-					headerOverlayTabs.forEach(tab => {
-						tab.style.background = `rgba(255, 255, 255, ${value})`;
-					});
-
-					await this.plugin.saveSettings();
-					this.plugin.updateBackgrounds();  // Reload backgrounds after changing opacity
-				}));
-
-		// Setting for tab header opacity
-		new Setting(containerEl)
-			.setName('Tab Header Opacity')
-			.setDesc('Opacity for the tab header background overlay.')
-			.addSlider(slider => slider
-				.setLimits(0, 1, 0.1)
-				.setValue(this.plugin.settings.opacitySettings.tabHeader)
-				.onChange(async (value) => {
-					this.plugin.settings.opacitySettings.tabHeader = value;
-
-					const workspaceOverlayLeaves = document.querySelectorAll(".workspace-leaf .background-overlay") as NodeListOf<HTMLElement>;
-					workspaceOverlayLeaves.forEach(leave => {
-						leave.style.background = `rgba(255, 255, 255, ${value})`;
-					});
-
-					await this.plugin.saveSettings();
-					this.plugin.updateBackgrounds();  // Reload backgrounds after changing opacity
-				}));
-
-		// Setting for workspace leaf opacity
-		new Setting(containerEl)
-			.setName('Workspace Leaf Opacity')
-			.setDesc('Opacity for the workspace leaf background overlay.')
-			.addSlider(slider => slider
-				.setLimits(0, 1, 0.1)
-				.setValue(this.plugin.settings.opacitySettings.workspaceLeaf)
-				.onChange(async (value) => {
-					this.plugin.settings.opacitySettings.workspaceLeaf = value;
-
-					const kanbanOverlayLanes = document.querySelectorAll(".kanban-plugin__lane .background-overlay") as NodeListOf<HTMLElement>;
-					kanbanOverlayLanes.forEach(lane => {
-						lane.style.background = `rgba(255, 255, 255, ${value})`;
-					});
-
-					await this.plugin.saveSettings();
-					this.plugin.updateBackgrounds();  // Reload backgrounds after changing opacity
-				}));
-
-		// Setting for kanban item title opacity
-		new Setting(containerEl)
-			.setName('Kanban Item Title Opacity')
-			.setDesc('Opacity for the Kanban item title background overlay.')
-			.addSlider(slider => slider
-				.setLimits(0, 1, 0.1)
-				.setValue(this.plugin.settings.opacitySettings.kanbanItem)
-				.onChange(async (value) => {
-					this.plugin.settings.opacitySettings.kanbanItem = value;
-
-					const kanbanOverlayItems = document.querySelectorAll(".kanban-plugin__item-title-wrapper .background-overlay") as NodeListOf<HTMLElement>;
-					kanbanOverlayItems.forEach(tab => {
-						console.log(tab, value);
-
-						tab.style.background = `rgba(255, 255, 255, ${value})`;
-					});
-
-					await this.plugin.saveSettings();
-					this.plugin.updateBackgrounds();  // Reload backgrounds after changing opacity
-				}));
+				})
+				.setDynamicTooltip());
 	}
 }
